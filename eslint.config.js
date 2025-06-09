58413c4ff0d074c4a23aa11754dd487a8ef105e5
Alvaro6800 <alvarolgsouza2009@gmail.com>
2025-06-09 14:31:31 -0300

    Feat: Adding pre commit

--- eslint.config.js
+++ eslint.config.js
@@ -3,24 +3,28 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import cypress from 'eslint-plugin-cypress'
+import prettier from 'eslint-config-prettier'
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
   {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-      'eslint-config-prettier',
-      'plugin: cypress/recommended',
-    ],
+    ignores: ['dist'],
+  },
+  {
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+      globals: {
+        ...globals.browser,
+      },
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      cypress,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -29,5 +33,10 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
     },
-  }
-)
+  },
+  // Configs como objetos (sem "extends")
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettier,
+  cypress.configs.recommended,
+]
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import {
   Header,
   CardComponent,
